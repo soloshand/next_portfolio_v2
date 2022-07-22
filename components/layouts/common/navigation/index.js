f5c42cbad51381a59b4ad761ad6d11e0b2724981
Stephen Fields <sfields0879@gmail.com>
2022-07-21 21:28:59 -0400

    Removed unecessary bootstrap containers (format)

--- components/layouts/common/navigation/index.js
+++ components/layouts/common/navigation/index.js
@@ -6,29 +6,25 @@ export default function NavBar() {
 
     const router = useRouter();
     return (
-        <div className="container-fluid">
-            <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                <div class="container-fluid">
-                    <div className="navbar-header">
-                        <a className="navbar-brand" href="/">
-                            Stephen Fields
-                        </a>
-                    </div>
-                    <ul className="nav navbar-nav">
-                        { navLinks.map((navitem, index) => {
-                            return(
-                                <li key={index}>
-                               <Link href={navitem.path}>
-                                   <a>{navitem.name}</a>
-                                </Link>
-                                </li>
-                            );
-                        }) 
-                            
-                        }
-                    </ul>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                <div className="navbar-header">
+                    <a className="navbar-brand" href="/">
+                        Stephen Fields
+                    </a>
                 </div>
-            </nav>
-        </div>
+                <ul className="nav navbar-nav">
+                    { navLinks.map((navitem, index) => {
+                        return(
+                            <li key={index}>
+                            <Link href={navitem.path}>
+                                <a>{navitem.name}</a>
+                            </Link>
+                            </li>
+                        );
+                    }) 
+                        
+                    }
+                </ul>
+        </nav>
      );
 }
