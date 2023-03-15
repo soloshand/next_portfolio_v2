07c1b5aa0e058391c2f952bf7bc348cabacd5bcb
Stephen Fields <sfields0879@gmail.com>
2023-03-15 08:56:28 -0400

    Removed nextjs image import

--- components/layouts/common/navigation/index.js
+++ components/layouts/common/navigation/index.js
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import Image from 'next/image';
 import {Container, Col, Row, Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
 import { navLinks } from "../../../../data/navigation"
 import { useRouter } from "next/router"
