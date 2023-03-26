d7d76124bc321a04d3a5f22d3855f71911492ab7
Stephen Fields <sfields0879@gmail.com>
2023-03-26 14:10:09 -0400

    removed api and install google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,30 @@
 import { useEffect } from 'react';
 import '../styles/index.css';
+import Script from 'next/script';
 
 function Portfolio({ Component, pageProps }) {
   
   return (
+    <>
+    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-11GV7TDXDB"/>
+    <Script
+      id='google-analytics'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{ 
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-11GV7TDXDB', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+    />
     <div className="container-fluid p-0">
       <Component {...pageProps} />
      </div>
+     </>
   )
 }
 
